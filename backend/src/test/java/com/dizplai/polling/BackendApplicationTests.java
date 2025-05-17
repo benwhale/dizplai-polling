057a5f1487cf0214e67bd0b5616cb7ba2fb62914
Ben Whale <benwhale@hotmail.co.uk>
2025-05-17 11:43:41 +0100

    added spring initialiser application

---
+++ backend/src/test/java/com/dizplai/polling/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dizplai.polling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
